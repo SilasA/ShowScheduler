5187e927191bb354014e4c966a77708c0898578f
Silas Agnew <agnewsilas@gmail.com>
2018-04-02 13:47:00 -0400

    Added more data to shows and started the scheduling algorithm.

--- ShowScheduler/Scheduler.cs
+++ ShowScheduler/Scheduler.cs
@@ -11,8 +11,9 @@ namespace ShowScheduler
     /// </summary>
     class Scheduler
     {
-        private Show[,] schedule;
+        public Show[,] Schedule { get; private set; }
 
+        private List<Show> shows;
 
         /// <summary>
         /// 
@@ -22,15 +23,46 @@ namespace ShowScheduler
             // 14 hours a day
             // 0 = morning slot
             // 1 - 14 are time - 9 = idx (first slot is 10:00)
-            schedule = new Show[7, 15]; // col x row
+            Schedule = new Show[7, 15]; // col x row
+
+            shows = Show.getShows();
         }
 
         /// <summary>
         /// TODO: Needs to run asynchronously
         /// </summary>
-        public void Schedule()
+        public void Generate()
         {
+            while (shows.Count > 0)
+            {
+                int pref = 0;
+                int[] slot = getSlot(pref, shows[0]);
+                if (Schedule[slot[0], slot[1]] == null)
+                {
+                    Schedule[slot[0], slot[1]] = shows[0];
+                    if (shows[0].TwoHour)
+                    {
+                        shows.RemoveAt(0);
+                        Schedule[slot[0], slot[1] + 1] = shows[0];
+                    }
+                    shows.RemoveAt(0);
+                }
+                else
+                {
 
+                }
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="pref"></param>
+        /// <param name="show"></param>
+        /// <returns></returns>
+        private int[] getSlot(int pref, Show show)
+        {
+            return new int[] { show.getTime(pref) - 9, show.getDay(pref) };
         }
     }
 }
--- ShowScheduler/Show.cs
+++ ShowScheduler/Show.cs
@@ -8,7 +8,30 @@ namespace ShowScheduler
     /// </summary>
     class Show
     {
-        public String Name { get; private set; }
+        /// <summary>
+        /// If the show is a 2 hour show
+        /// </summary>
+        public bool TwoHour { get; private set; }
+
+        /// <summary>
+        /// If this is a dummy show (for 2 hr shows)
+        /// </summary>
+        public bool Dummy { get; private set; }
+
+        /// <summary>
+        /// Parent show if this is a dummy
+        /// </summary>
+        public Show Parent { get; private set; }
+
+        /// <summary>
+        /// Title of the Show
+        /// </summary>
+        private String name;
+
+        /// <summary>
+        /// Semesters in the org.
+        /// </summary>
+        private int tenure;
 
         // time from 10 to 23
         private List<int> times;
@@ -21,25 +44,83 @@ namespace ShowScheduler
         /// <param name="name"></param>
         /// <param name="time"></param>
         /// <param name="day"></param>
-        public Show(String name, List<int> time, List<int> day)
+        public Show(String name, List<int> time, List<int> day, int tenure, bool twoHour)
         {
-            Name = name;
-            times = time;
-            days = day;
+            this.name = name;
+            this.tenure = tenure;
+            this.times = time;
+            this.days = day;
+            TwoHour = twoHour;
+            Dummy = false;
+            Parent = null;
         }
 
         /// <summary>
         /// 
         /// </summary>
-        /// <param name="rank">Preference of time (0 being prefered)</param>
-        /// <returns></returns>
-        public int getTime(int rank) { return times[rank]; }
+        /// <param name="parent"></param>
+        public Show(Show parent)
+        {
+            Dummy = true;
+            Parent = parent;
+            TwoHour = true;
+        }
+
+        #region Accessors
 
         /// <summary>
         /// 
         /// </summary>
-        /// <param name="rank">Preference of time (0 being prefered)</param>
         /// <returns></returns>
-        public int getDay(int rank) { return days[rank]; }
+        public String getName()
+        {
+            if (Dummy) return Parent.name;
+            return name;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public int getTenure()
+        {
+            if (Dummy) return Parent.tenure;
+            return tenure;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="rank">Preference of time (0 being preferred)</param>
+        /// <exception cref="IndexOutOfRangeException"></exception>
+        /// <returns></returns>
+        public int getTime(int rank)
+        {
+            if (Dummy) return Parent.times[rank];
+            return times[rank];
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="rank">Preference of time (0 being preferred)</param>
+        /// <exception cref="IndexOutOfRangeException"></exception>
+        /// <returns></returns>
+        public int getDay(int rank)
+        {
+            if (Dummy) return Parent.days[rank];
+            return days[rank];
+        }
+
+        #endregion
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public static List<Show> getShows()
+        {
+            return new List<Show>();
+        }
     }
 }
