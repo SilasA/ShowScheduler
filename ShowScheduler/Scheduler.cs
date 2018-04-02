38baa9c1d874bb930e510478a328ae468166cfd3
Silas Agnew <agnewsilas@gmail.com>
2018-04-02 00:29:08 -0400

    Added some outline files for the idea

---
+++ ShowScheduler/Scheduler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShowScheduler
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    class Scheduler
+    {
+        private Show[,] schedule;
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Scheduler()
+        {
+            // 14 hours a day
+            // 0 = morning slot
+            // 1 - 14 are time - 9 = idx (first slot is 10:00)
+            schedule = new Show[7, 15]; // col x row
+        }
+
+        /// <summary>
+        /// TODO: Needs to run asynchronously
+        /// </summary>
+        public void Schedule()
+        {
+
+        }
+    }
+}
---
+++ ShowScheduler/Show.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShowScheduler
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    class Show
+    {
+        public String Name { get; private set; }
+
+        // time from 10 to 23
+        private List<int> times;
+        // day from 0 to 6 (0 monday)
+        private List<int> days;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="time"></param>
+        /// <param name="day"></param>
+        public Show(String name, List<int> time, List<int> day)
+        {
+            Name = name;
+            times = time;
+            days = day;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="rank">Preference of time (0 being prefered)</param>
+        /// <returns></returns>
+        public int getTime(int rank) { return times[rank]; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="rank">Preference of time (0 being prefered)</param>
+        /// <returns></returns>
+        public int getDay(int rank) { return days[rank]; }
+    }
+}
